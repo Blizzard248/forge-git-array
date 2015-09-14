a9e39bbd8a615aea0f9e0e8da1c30e8f73fe2cde
Oscar Maciel <blizzard248@gmail.com>
2015-09-14 18:47:19 -0300

    Cosa

--- array_utils.rb
+++ array_utils.rb
@@ -1,11 +1,10 @@
 module ArrayUtils
 	def self.array_max(param_array)
-		max = param_array
+		max = param_array[0]
 
 		param_array.each do |element|
-			max = element if element > max
+			if element > max ? element : max
 		end
-
 		max
 	end
 end
